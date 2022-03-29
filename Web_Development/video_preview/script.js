c6ebe5e44555dbc64ce66b32bf4b6d367b788c03
James Luty <luty0125@gmail.com>
2022-03-28 17:17:29 -0700

    uploading video preview folder

---
+++ Web_Development/video_preview/script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+var vid = document.getElementById("beach");
+
+function start() {
+    // console.log("over");
+    vid.play();
+}
+
+function end() {
+    // console.log("off");
+    vid.pause();
+}
