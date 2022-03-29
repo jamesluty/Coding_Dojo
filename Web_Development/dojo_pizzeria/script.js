5815cdf873270e1d2d7b4c2a0c998abadb340fdf
James Luty <luty0125@gmail.com>
2022-03-29 09:57:32 -0700

    uploading pizzeria js and adding pokemon js

---
+++ Web_Development/dojo_pizzeria/script.js
@@ -0,0 +1,36 @@
+var options = {
+    crust: ["deep dish", "handtossed", "thin crust"],
+    sauce: ["traditional", "marinara"],
+    cheese: ["mozzarella", "feta", "cheedar"],
+    toppings: ["pepperoni", "sausage", "mushrooms", "olives", "onions", "bacon"]
+}
+
+function pizzaOven(crust, sauce, cheese, toppings){
+    var pizza = {};
+    pizza.crust = crust;
+    pizza.sauce = sauce;
+    pizza.cheese = cheese;
+    pizza.toppings = toppings;
+    return pizza;    
+}
+
+function rand(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+console.log(rand(options));
+
+var pizza1 = pizzaOven("deep dish", "traditional", "mozzarella", ["pepperoni", "sausage"]);
+console.log(pizza1);
+
+var pizza2 = pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]);
+console.log(pizza2);
+
+var pizza3 = pizzaOven("deep dish", "traditional", "mozzarella");
+console.log(pizza3);
+
+var pizza4 = pizzaOven("thin crust", "marinara", "feta", ["pepperoni", "mushrooms"]);
+console.log(pizza4);
+
+var randPizza = pizzaOven(options.crust[rand(options.crust)], options.sauce[rand(options.sauce)], options.cheese[rand(options.cheese)], options.toppings[rand(options.toppings)]);
+console.log(randPizza);
