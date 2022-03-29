c9990dbed189e1755bcb2a92e9c73592e8447f86
James Luty <luty0125@gmail.com>
2022-03-29 16:35:14 -0700

    updated like folder to use one function to change like buttons

--- Web_Development/likes/script.js
+++ Web_Development/likes/script.js
@@ -1,21 +1,6 @@
-var add1 = document.querySelector("#likes1");
-var add2 = document.querySelector("#likes2");
-var add3 = document.querySelector("#likes3");
-var count1 = 0
-var count2 = 0
-var count3 = 0
-
-function addLike(){
-    count1++
-    add1.innerText = count1 + " like(s)"
-}
-
-function addLike2(){
-    count2++
-    add2.innerText = count2 + " like(s)"
-}
-
-function addLike3(){
-    count3++
-    add3.innerText = count3 + " like(s)"
+function addLike(elem){
+    var id = document.querySelector(elem);
+    x = id.innerText.split(" ");
+    var count = parseInt(x[0]);
+    id.innerText = ++count + " like(s)"
 }
