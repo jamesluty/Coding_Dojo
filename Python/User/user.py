0a77a3bdcec13540acbecd7e7414e164845fa54a
James Luty <luty0125@gmail.com>
2022-04-05 13:08:12 -0700

    chained class methods together

--- Python/User/user.py
+++ Python/User/user.py
@@ -8,14 +8,15 @@ class User:
 
     def make_deposit(self, amount):
         self.balance += amount
-        return amount
+        return self
 
     def make_withdrawal(self, withdraw):
         self.balance -= withdraw
-        return withdraw
+        return self
     
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.balance}")
+        return self
 
     def transfer_money(self, otherUser, amount):
         otherUser.make_deposit(amount)
@@ -28,27 +29,24 @@ matt = User("Matty Matt", "monty@python.com", "9/6/1990")
 james = User("James Luty", "jluty@python.com", "1/25/1986")
 
 # Making deposits to accounts
-oliver.make_deposit(100)
-oliver.make_deposit(200)
-oliver.make_deposit(500)
-matt.make_deposit(250)
-james.make_deposit(2000000)
+oliver.make_deposit(100).make_deposit(200).make_deposit(500).make_withdrawal(100).display_user_balance()
+matt.make_deposit(250).make_withdrawal(20).make_withdrawal(100).display_user_balance()
+james.make_deposit(2000000).make_withdrawal(20000).display_user_balance().transfer_money(matt, 500)
 
 # print(oliver.balance)
 # print(matt.balance)
 
 # Making withdrawls from accounts
-oliver.make_withdrawal(100)
-matt.make_withdrawal(20)
-matt.make_withdrawal(100)
-james.make_withdrawal(20000)
+# oliver.make_withdrawal(100)
+# matt.make_withdrawal(20).make_withdrawal(100)
+# james.make_withdrawal(20000)
 
 # print(oliver.balance)
 # print(matt.balance)
 # print(james.balance)
 
 # Transfer
-james.transfer_money(matt, 500)
+# james.transfer_money(matt, 500)
 
 # oliver.display_user_balance()
 # matt.display_user_balance()
