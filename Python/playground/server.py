a29ef2e84462343246f812d588338bb093a52d5d
James Luty <luty0125@gmail.com>
2022-04-07 15:43:33 -0700

    uploading playground folder

---
+++ Python/playground/server.py
@@ -0,0 +1,13 @@
+from sys import builtin_module_names
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/play')
+@app.route('/play/<int:num>')
+@app.route('/play/<int:num>/<string:color>')
+def play_color(num=3, color="blue"):
+    return render_template("index.html", num=num, color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)
