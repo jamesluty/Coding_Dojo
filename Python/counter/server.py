caab1f6a5cb206f0cb719dd2e86f58c8e408b053
James Luty <luty0125@gmail.com>
2022-04-10 17:32:30 -0700

    fixed issue with sessiont count variable

--- Python/counter/server.py
+++ Python/counter/server.py
@@ -5,29 +5,30 @@ app.secret_key = "keep it secret, keep it safe"
 
 @app.route('/')
 def index():
-    count = 1
-    session['count'] += count
+    if session.get('count') is None:
+        count = 1
+        session['count'] = count
     return render_template("index.html", count=session['count'])
 
 @app.route('/count')
 def add_one():
+    session['count'] += 1
     return redirect('/')
 
 @app.route('/two')
 def add_two():
-    session['count'] += 1
+    session['count'] += 2
     return redirect('/')
 
 @app.route('/input', methods=['POST'])
 def add_input():
     amount = request.form['amount']
-    session['count'] += int(amount) - 1
+    session['count'] += int(amount)
     return redirect('/')
 
 @app.route('/clear')
 def clear():
     session.clear()
-    session['count']=0
     return redirect("/")
 
 if __name__=="__main__":
