31f72d0a1527597913c7fda9d8b2686142927a62
James Luty <luty0125@gmail.com>
2022-04-11 10:01:48 -0700

    fixed indentation

--- Python/algos/week_2/w2d1.js
+++ Python/algos/week_2/w2d1.js
@@ -1,24 +1,24 @@
 /* 
-  Given an array of strings
-  return a sum to represent how many times each array item is found (a frequency table)
-  Useful methods:
+    Given an array of strings
+    return a sum to represent how many times each array item is found (a frequency table)
+    Useful methods:
     Object.hasOwnProperty("keyName")
-      - returns true or false if the object has the key or not
+        - returns true or false if the object has the key or not
     Python: key in dict
 */
 
 const arr1 = ["a", "a", "a"];
 const expected1 = {
-  a: 3,
+    a: 3,
 };
 
 const arr2 = ["a", "b", "a", "c", "B", "c", "c", "d"];
 const expected2 = {
-  a: 2,
-  b: 1,
-  c: 3,
-  B: 1,
-  d: 1,
+    a: 2,
+    b: 1,
+    c: 3,
+    B: 1,
+    d: 1,
 };
 
 const arr3 = [];
