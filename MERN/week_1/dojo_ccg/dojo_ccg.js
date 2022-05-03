8d89e601384ad26fd3bbc554a39e8f2c173ea5ef
James Luty <luty0125@gmail.com>
2022-05-03 14:00:15 -0700

    changes to console logs and remove cost

--- MERN/week_1/dojo_ccg/dojo_ccg.js
+++ MERN/week_1/dojo_ccg/dojo_ccg.js
@@ -13,8 +13,12 @@ class Unit extends Card {
     }
 
     attack(target) {
-        target.resilience -= this.power;
-        console.log(this.name + " attacked " + target.name + " and lowered their resilience to " + target.resilience);
+        if ( target instanceof Unit){
+            target.resilience -= this.power;
+            console.log(`${ this.name} attacked ${target.name} and lowered their resilience by ${target.resilience}`);
+        } else {
+            throw new Error(`${target} is not a unit`);
+        }
     }
 }
 
@@ -27,18 +31,25 @@ class Effect extends Card {
     }
 
     play( unit ) {
-        unit.cost -= this.cost
-        unit.resilience = this.magnitude;
-        console.log(unit.name + " played " + this.name + " for " + this.cost + " gold to change " + this.stat + " by " + this.magnitude );
+        if ( unit instanceof Unit){
+            if (this.stat === "power"){
+                unit.power += this.magnitude
+            } else if (this.stat === "resilience") {
+                unit.resilience += this.magnitude
+            }
+            console.log(`${unit.name} played ${this.name} to ${this.text}`);
+        } else {
+            throw new Error(`${unit} is not a valid unit`);
+        }
     }
 }
 
 let unit_red = new Unit("Red Belt Ninja", 3, 3, 4);
 let unit_black = new Unit("Black Belt Ninja", 4, 5, 4);
 
-let effect_hard_algo = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", +3);
+let effect_hard_algo = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", 3);
 let effect_unhandled_promise_rej = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
-let effect_pair_programming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", +2);
+let effect_pair_programming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", 2);
 
 effect_hard_algo.play(unit_red);
 effect_unhandled_promise_rej.play(unit_red);
