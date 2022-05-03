4aebad504d4baf716b8968a7e9ebd936ee64569f
James Luty <luty0125@gmail.com>
2022-05-02 19:49:42 -0700

    add Sensei class extension to Ninja

--- MERN/week_1/Ninja/ninja.js
+++ MERN/week_1/Ninja/ninja.js
@@ -8,12 +8,15 @@ class Ninja {
 
     // sayName method
     sayName() {
-        console.log("Name: ${this.name}.");
+        console.log("Name: " + this.name);
     }
 
     // showStats method
     showStats() {
-        console.log("Name: ${this.name}, Strength: ${this.stength}, Speed: %{this.speed}, Health: ${this.health}");
+        console.log("Name: " + this.name);
+        console.log("Strength: " + this.strength);
+        console.log("Speed: " + this.speed);
+        console.log("Health: " + this.health);
     }
 
     // drinkSake() method
@@ -21,3 +24,24 @@ class Ninja {
         this.health += 10;
     }
 }
+
+const ninja1 = new Ninja("Hyabusa", 100)
+ninja1.sayName();
+ninja1.showStats();
+
+class Sensei extends Ninja {
+    constructor(name, health=200, speed=10, strength=10, wisdom=10) {
+        super(name, health, speed, strength);
+        this.wisdom = wisdom;
+    }
+
+    // speakWisdom method
+    speakWisdom() {
+        super.drinkSake();
+        console.log("A setback is just a setup for a comeback");
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
