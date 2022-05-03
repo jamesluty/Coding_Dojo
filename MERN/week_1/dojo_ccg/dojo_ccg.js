fbaf83738a62c41f5e9973677d322a415db6090c
James Luty <luty0125@gmail.com>
2022-05-02 20:31:12 -0700

    fixed formatting

--- MERN/week_1/dojo_ccg/dojo_ccg.js
+++ MERN/week_1/dojo_ccg/dojo_ccg.js
@@ -14,7 +14,7 @@ class Unit extends Card {
 
     attack(target) {
         target.res -= this.power;
-        console.log(this.name + " attacked " + target.name + " and lowered their resilience to " + target.res)
+        console.log(this.name + " attacked " + target.name + " and lowered their resilience to " + target.res);
     }
 }
 
@@ -23,12 +23,12 @@ class Effect extends Card {
         super(name, cost);
         this.text = text;
         this.stat = stat;
-        this.magnitude = magnitude
+        this.magnitude = magnitude;
     }
 
     play( unit ) {
-        unit.res = this.magnitude
-        console.log(unit.name + " increase their resilience by " + this.magnitude)
+        unit.res = this.magnitude;
+        console.log(unit.name + " increase their resilience by " + this.magnitude);
     }
 }
 
@@ -37,9 +37,9 @@ let unit_black = new Unit("Black Belt Ninja", 4, 5, 4);
 
 let effect_hard_algo = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", +3);
 let effect_unhandled_promise_rej = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience", -2);
-let effect_pair_programming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", +2)
+let effect_pair_programming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", +2);
 
-effect_hard_algo.play(unit_red)
-effect_unhandled_promise_rej.play(unit_red)
-effect_pair_programming.play(unit_red)
-unit_red.attack(unit_black)
+effect_hard_algo.play(unit_red);
+effect_unhandled_promise_rej.play(unit_red);
+effect_pair_programming.play(unit_red);
+unit_red.attack(unit_black);
